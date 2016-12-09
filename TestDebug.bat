47c6ab97f35a023872215d6b102ea0825fbf029d
Joel Christner <joel@maraudersoftware.com>
2016-12-09 09:33:50 -0800

    Added test batch file

---
+++ TestDebug.bat
@@ -0,0 +1,21 @@
+@echo off
+IF [%1] == [] GOTO Usage
+cd TestServer\bin\debug
+start TestServer.exe
+TIMEOUT 3 > NUL
+cd ..\..\..
+
+cd TestClient\bin\debug
+FOR /L %%i IN (1,1,%1) DO (
+ECHO Starting client %%i
+start TestClient.exe
+TIMEOUT 1 > NUL
+)
+cd ..\..\..
+@echo on
+EXIT /b
+
+:Usage
+ECHO Specify the number of client nodes to start.
+@echo on
+EXIT /b
