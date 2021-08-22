a09ba2d65031f069ec1ed6349eb5950fe75f618a
cee-sharp <89317478+cee-sharp@users.noreply.github.com>
2021-08-22 12:07:41 +0200

    Implemented property for setting up TLS version (.Net 5.0 or greater)

---
+++ WatsonTcp/TlsExtensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Security.Authentication;
+
+namespace WatsonTcp
+{
+    static class TlsExtensions
+    {
+        public static SslProtocols ToSslProtocols(this TlsVersion tlsVersion)
+        {
+            switch (tlsVersion)
+            {
+                case TlsVersion.Tls12:
+                    return SslProtocols.Tls12;
+#if NET5_0_OR_GREATER
+                case TlsVersion.Tls13:
+                    return SslProtocols.Tls13;
+#endif
+                default: 
+                    throw new ArgumentOutOfRangeException($"Unsupported TLS version {tlsVersion}");
+            }
+        }
+    }
+}
---
+++ WatsonTcp/TlsVersion.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace WatsonTcp
+{
+    /// <summary>
+    /// Supported TLS versions
+    /// </summary>
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TlsVersion
+    {
+        /// <summary>
+        /// Use TLS 1.2 (you can't go below this)
+        /// </summary>
+        Tls12,
+        /// <summary>
+        /// Use TLS 1.3 (only valid for .Net 5.0 or greater)
+        /// </summary>
+        Tls13,
+    }
+}
