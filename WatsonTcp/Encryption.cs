02d24e72f22d00f31598e3e1bb1c2d2338d66e17
Justin Lopez <developer.variety@gmail.com>
2021-12-26 16:10:24 -0500

    Hide passphrase from message headers

--- WatsonTcp/Encryption.cs
+++ WatsonTcp/Encryption.cs
@@ -6,14 +6,16 @@ namespace WatsonTcp
     {
         #region Public-Members
         /// <summary>
-        /// Passphrase that must be consistent between clients and this server for encrypted communication.
-        /// </summary>
-        public string Passphrase = string.Empty;
-
-        /// <summary>
         /// Algorithm mechanism used to encrypt payload between clients and this server for encrypted communication.
         /// </summary>
         public EncryptionType Algorithm = EncryptionType.None;
+        #endregion
+
+        #region Internal-Members
+        /// <summary>
+        /// Passphrase that must be consistent between clients and this server for encrypted communication.
+        /// </summary>
+        internal string Passphrase = string.Empty;
         #endregion
     }
 }
