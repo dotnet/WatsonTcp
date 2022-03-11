706bb73ef28df8077358652f831ff3a1a7a778e1
The Chosen Sausage <the.chosen.sausage.0510@gmail.com>
2022-03-11 20:04:51 +0800

    Add SyncResponseReceivedEventArgs

---
+++ WatsonTcp/SyncResponseReceivedEventArgs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WatsonTcp
+{
+    /// <summary>
+    /// Internal EventArgs for passing arguments for SyncResponseReceived event.
+    /// </summary>
+    internal class SyncResponseReceivedEventArgs
+    {
+        public SyncResponseReceivedEventArgs(WatsonMessage msg, byte[] data)
+        {
+            message = msg;
+            Data = data;
+        }
+        public WatsonMessage message;
+        public byte[] Data;
+    }
+}
