46c0cd55261d174b80815e5a6f598da14b10dd6c
Joel Christner <joel.christner@gmail.com>
2022-03-21 12:22:52 -0700

    Test.Metadata project update

--- Test.Metadata/Program.cs
+++ Test.Metadata/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
@@ -9,28 +10,63 @@ namespace Test.Metadata
 {
     class Program
     {
+        static int _ServerPort = 8000;
+        static int _MessageCount = 1;
+        static int _EntryCount = 500;
+        static Stopwatch[] _Stopwatches = null;
+
         static void Main(string[] args)
         {
-            using (WatsonTcpServer server = new WatsonTcpServer("127.0.0.1", 8000))
+            if (args != null && args.Length == 2)
             {
+                _MessageCount = Convert.ToInt32(args[0]);
+                _EntryCount = Convert.ToInt32(args[1]);
+            }
+
+            _Stopwatches = new Stopwatch[_MessageCount];
+
+            using (WatsonTcpServer server = new WatsonTcpServer("127.0.0.1", _ServerPort))
+            {
+                // server.Settings.DebugMessages = true;
+                // server.Settings.Logger = ServerLogger;
                 server.Events.MessageReceived += ServerMessageReceived;
                 server.Start();
+                Console.WriteLine("Server started");
                 Task.Delay(1000).Wait();
-                using (WatsonTcpClient client = new WatsonTcpClient("127.0.0.1", 8000))
+
+                using (WatsonTcpClient client = new WatsonTcpClient("127.0.0.1", _ServerPort))
                 {
                     client.Events.MessageReceived += ClientMessageReceived;
                     client.Connect();
+                    Console.WriteLine("Client connected to server");
 
-                    for (int i = 0; i < 10; i++)
+                    for (int i = 0; i < _MessageCount; i++)
                     {
-                        Person p = new Person("hello", "world", i);
                         Dictionary<object, object> md = new Dictionary<object, object>();
-                        md.Add("person", p);
-                        client.Send(("Message " + i), md);
-                        Task.Delay(1000).Wait();
+
+                        for (int j = 0; j < _EntryCount; j++)
+                        {
+                            Person p = new Person("hello", "world", i.ToString() + "." + j.ToString());
+                            md.Add("person." + i.ToString() + "." + j.ToString(), p);
+                        }
+
+                        client.Send(i.ToString(), md);
+
+                        _Stopwatches[i] = new Stopwatch();
+                        _Stopwatches[i].Start();
+
+                        Console.WriteLine("Client sent message " + i);
                     }
                 }
+
+                Console.WriteLine("Press ENTER to exit");
+                Console.ReadLine();
             }
+        }
+
+        private static void ServerLogger(Severity arg1, string arg2)
+        {
+            Console.WriteLine(arg1.ToString() + ": " + arg2);
         }
 
         #region Events
@@ -39,10 +75,11 @@ namespace Test.Metadata
         { 
             try
             {
-                object o = args.Metadata["person"];
-                Console.WriteLine(SerializeJson(o, true));
-                Person p = (Person)o; 
-                Console.WriteLine(p.FirstName + " " + p.LastName + ": " + p.Number);
+                int msgNum = Convert.ToInt32(Encoding.UTF8.GetString(args.Data));
+                Dictionary<object, object> md = args.Metadata;
+                _Stopwatches[msgNum].Stop();
+                Console.WriteLine("Server received message " + msgNum.ToString() + " with " + md.Count + " metadata entries: " + _Stopwatches[msgNum].ElapsedMilliseconds + "ms");
+
             }
             catch (Exception e)
             {
@@ -107,18 +144,18 @@ namespace Test.Metadata
     {
         public string FirstName { get; set; }
         public string LastName { get; set; }
-        public int Number { get; set; }
+        public string Identifier { get; set; }
 
         public Person()
         {
 
         }
 
-        public Person(string first, string last, int num)
+        public Person(string first, string last, string id)
         {
             FirstName = first;
             LastName = last;
-            Number = num;
+            Identifier = id;
         }
     }
 }
